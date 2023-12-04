ae4028e76efc35f22c59282e7fc9851a6c3186dd
KILLinefficiency <shreyas.sable2166@gmail.com>
2023-12-04 17:39:19 +0530

    aoc day 4 part 2 done

--- AoC/2023/Dart/Day04/2.dart
+++ AoC/2023/Dart/Day04/2.dart
@@ -1,3 +1,31 @@
+import "dart:io";
+import "dart:math";
+
+Set<int> makeSet(String text) {
+    return text.split(" ").where((x) => x != "").map((x) => int.parse(x)).toSet();
+}
+
+List<String> lines = File("input.txt").readAsStringSync().trim().split("\n");
+
+List<int> getCopy(int index) {
+    List<String> tokens = lines[index - 1].split(RegExp(r"[:|]")).map((x) => x.trim()).toList();
+    Set<int> winning = makeSet(tokens[1]);
+    Set<int> your = makeSet(tokens[2]);
+    int won = winning.intersection(your).toList().length;
+    return [for(int x = index + 1; x <= won + index; x++) x];
+}
+
 void main() {
-    print("Hello AoC");
+    int answer = 0;
+    List<List<int>> data = [[for(int n = 1; n <= lines.length; n++) n]];
+    for(int i = 0; i < data.length; i++) {
+        for(int j = 0; j < data[i].length; j++) {
+            data.add(getCopy(data[i][j]));
+        }
+    }
+
+    for(List<int> each in data) {
+        answer += each.length;
+    }
+    print(answer);
 }
